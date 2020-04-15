cfd541b96fe590768daa2b82b4087bc3aa076e34
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-04-15 15:56:57 -0300

    Setting up database (via MongAtlas DB) connection.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const CONNECTION = process.env.MONGO_DB_URI;
+
+mongoose.connect(CONNECTION, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('DB connected');
+}).catch((err) => {
+    console.log(`Error connecting to the database:\n ${err}`)
+});
 
 app.get('/', (req, res) => {
     res.send('Hello from NodeJS!');
