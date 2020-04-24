82443d737fe3e32d1271c4dd6c3d328cee477bb9
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-04-24 16:32:28 -0300

    Created update route for product.

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -105,3 +105,51 @@ exports.removeAllProducts = (req, res) => {
         }
     })
 };
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+
+    form.keepExtensions = true;
+
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded' + err
+            });
+        } else {
+            let product = req.product;
+            let { name, description, price, category, quantity, shipping } = fields;
+            let photo = files.photo;
+
+            product = _.extend(product, fields);
+
+            // validations
+            if (photo) {
+                if (files.photo.size > 1000000) {
+                    return res.status(400).json({
+                        error: 'Image should be less than 1mb in size'
+                    });
+                }
+
+                product.photo.data = fs.readFileSync(photo.path);
+                product.photo.contentType = photo.type;
+            }
+
+            if (!name || !description || !price || !category || !quantity || !shipping) {
+                return res.status(400).json({
+                    error: 'All files are required'
+                });
+            }
+
+            product.save((err, data) => {
+                if (err) {
+                    return res.status(400).json({
+                        error: errorHandler(err)
+                    });
+                } else {
+                    res.json(data);
+                }
+            });
+        }
+    })
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {create, remove, removeAllProducts, read, findProductById} = require('../controllers/product');
+const {create, remove, removeAllProducts, update, read, findProductById} = require('../controllers/product');
 const {findUserById} =  require('../controllers/user');
 const {requireSignIn, isAuth, isAdmin} =  require('../controllers/auth');
 
 router.post('/product/create/:userId', requireSignIn, isAuth, isAdmin, create);
 router.get('/product/:productId', read);
 router.delete('/product/destroy/:userId', requireSignIn, isAuth, isAdmin, removeAllProducts);
+router.put('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, update);
 router.delete('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, remove);
 
 
