f3f907f22791d5caa7f7d133855f5d955d7692c6
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-05-04 21:38:38 -0300

    Installed CORS package, to handle incoming frontend requisitions in the project.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 require('dotenv').config();
 // import routes
@@ -31,6 +32,7 @@ mongoose.connect(CONNECTION, {
 app.use(morgan('D'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(expressValidator());
 
 // routes
