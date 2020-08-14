f503714b2c787db5c2c841bb910dbbb1f842657b
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-13 21:31:20 -0300

    Created payment resource.

--- src/controllers/braintree.js
+++ src/controllers/braintree.js
@@ -18,3 +18,21 @@ exports.generateToken = (req, res) => {
         }
     });
 };
+
+exports.processPaymeny = (req, res) => {
+    let nonceFromTheClient = req.body.paymentMethodNonce;
+    let amountFromTheClient = req.body.amount;
+    let newTransaction = gateway.transaction.sale({
+        amount: amountFromTheClient,
+        paymentMethodNonce: nonceFromTheClient,
+        options: {
+            submitForSettlement: true
+        }
+    }, (error, result) => {
+        if(error) {
+            res.status(500).json(error);
+        } else {
+            res.json(result);
+        }
+    });
+};
--- src/routes/braintree.js
+++ src/routes/braintree.js
@@ -5,6 +5,7 @@ const {findUserById} =  require('../controllers/user');
 const {requireSignIn, isAuth} =  require('../controllers/auth');
 
 router.get('/braintree/getToken/:userId', requireSignIn, isAuth, generateToken);
+router.post('/braintree/payment/:userId', requireSignIn, isAuth, processPayment);
 router.param('userId', findUserById);
 
 module.exports = router;
