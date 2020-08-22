0e0164ac64b51468fc48b2c7e7f8a8851dd9fa77
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-22 19:10:43 -0300

    Created middleware to update product quantity and sold information when processing new order.

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -301,3 +301,27 @@ exports.listSearch = (req, res) => {
         }).select('-photo');    
     }
 };
+
+exports.decreaseProductQuantity = (req, res, next) => {
+    let bulkOps = req.body.order.products.map((item) => {
+        return{
+            updateOne: {
+                filter: {_id: item._id},
+                update: {$inc: {
+                    quantity: -item.count,
+                    sold: +item.count
+                }}
+            }
+        };
+    });
+
+    Product.bulkWrite(bulkOps, {}, (error, products) => {
+        if (error) {
+            return res.status(400).json({
+                error: 'Could not update product'
+            });
+        }
+
+        next();
+    })
+};
--- src/routes/order.js
+++ src/routes/order.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const {findUserById, addOrderToUserHistory} =  require('../controllers/user');
 const {requireSignIn, isAuth} =  require('../controllers/auth');
 const {create} =  require('../controllers/order');
+const {decreaseProductQuantity} =  require('../controllers/product');
 
-router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToUserHistory, create);
+router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToUserHistory, decreaseProductQuantity, create);
 
 router.param('userId', findUserById);
 
