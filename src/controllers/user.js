ac12d5b5350cb42b9118b5820de0b03a5c5ff213
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-22 18:45:42 -0300

    Created resource in User controller to save user pruchase history.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -39,3 +39,34 @@ exports.update = (req, res) => {
             }
         )
 };
+
+exports.addOrderToUserHistory = (req, res, next) => {
+    let history = [];
+
+    req.body.order.products.forEach((item) => {
+        history.push({
+            _id: item._id,
+            name: item.name,
+            description: item.description,
+            category: item.category,
+            quantity: item.count,
+            transaction_id: req.body.order.transaction_id,
+            amount: req.body.order.amount
+        });      
+    });
+
+    User.findOneAndUpdate(
+        {_id: req.profile._id},
+        {$push: {history: history}},
+        {new: true},
+        (err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Error updating user purchase history'
+                })
+            }
+
+            next();
+        }
+    );
+};
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {findUserById} =  require('../controllers/user');
+const {findUserById, addOrderToUserHistory} =  require('../controllers/user');
 const {requireSignIn, isAuth} =  require('../controllers/auth');
 const {create} =  require('../controllers/order');
 
-router.post('/order/create/:userId', requireSignIn, isAuth, create);
+router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToUserHistory, create);
 
 router.param('userId', findUserById);
 
