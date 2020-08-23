d5b791d885bda461bc7351ef2876bbd0c1d9c410
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-23 19:02:17 -0300

    Created route to list all order status values.

--- src/routes/order.js
+++ src/routes/order.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {findUserById, addOrderToUserHistory} =  require('../controllers/user');
 const {requireSignIn, isAuth, isAdmin} =  require('../controllers/auth');
-const {create, listOrders} =  require('../controllers/order');
+const {create, listOrders, getStatusValues} =  require('../controllers/order');
 const {decreaseProductQuantity} =  require('../controllers/product');
 
 router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToUserHistory, decreaseProductQuantity, create);
 router.get('/order/list/:userId', requireSignIn, isAuth, isAdmin, listOrders);
+router.get('/order/status-values/:userId', requireSignIn, isAuth, isAdmin, getStatusValues);
 
 router.param('userId', findUserById);
 
