69219aae6972c5579d04f4cbf93693a5f0cde8c7
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-22 23:35:44 -0300

    Created list method for Orders resource.

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {findUserById, addOrderToUserHistory} =  require('../controllers/user');
-const {requireSignIn, isAuth} =  require('../controllers/auth');
-const {create} =  require('../controllers/order');
+const {requireSignIn, isAuth, isAdmin} =  require('../controllers/auth');
+const {create, listOrders} =  require('../controllers/order');
 const {decreaseProductQuantity} =  require('../controllers/product');
 
 router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToUserHistory, decreaseProductQuantity, create);
+router.get('/order/list/:userId', requireSignIn, isAuth, isAdmin, listOrders);
 
 router.param('userId', findUserById);
 
