74d9a23a01d31c194ff2c967714465c939529a50
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-08-26 21:55:28 -0300

    Created route to list user's purchase history (orders).

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {findUserById, read, update} =  require('../controllers/user');
+const {findUserById, read, update, purchaseHistory} =  require('../controllers/user');
 const {requireSignIn, isAuth, isAdmin} =  require('../controllers/auth');
 
 router.get('/secret/:userId', requireSignIn, isAuth, isAdmin, (req, res) => {
@@ -11,5 +11,7 @@ router.get('/secret/:userId', requireSignIn, isAuth, isAdmin, (req, res) => {
 router.param('userId', findUserById);
 router.get('/user/:userId', requireSignIn, isAuth, read);
 router.put('/user/:userId', requireSignIn, isAuth, update);
+router.get('/orders/by/user/:userId', requireSignIn, isAuth, purchaseHistory);
+
 
 module.exports = router;
